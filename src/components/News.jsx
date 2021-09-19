721b034cd8a40be7e2a1cdbdd6fb45431f9a0668
Adityanarayan Tiwari <adityant17121997@gmail.com>
2021-09-19 21:23:16 +0530

    Truncate Logic Updated

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,11 +3,11 @@ import NewsItem from './NewsItem';
 
 export class News extends Component {
 
-  async componentDidMount(){
+  async componentDidMount() {
     let url = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e86cf0ce88114c588fe0cca9a23ee0c0';
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({articles : parsedData.articles})
+    this.setState({ articles: parsedData.articles })
   }
 
   constructor() {
@@ -17,19 +17,21 @@ export class News extends Component {
       loading: false
     }
   }
+
   render() {
-    let defaultImageUrl='https://images.wsj.net/im-403222/social';
+    let defaultImageUrl = 'https://images.wsj.net/im-403222/social';
     return (
       <div className='container mt-3'>
         <h1 className='mb-5'>NewsWorld - Top Headlines</h1>
         <div className="row row-cols-1 row-cols-md-3 g-4">
-          { 
-            this.state.articles.map((element) => { 
-              return <NewsItem key={element.url} 
-              title={element.title ?  element.title.slice(0, 30) : 'NA'} 
-              description={element.description ?  element.description.slice(0, 88) : 'NA'} 
-              imageUrl={element.urlToImage ? element.urlToImage : defaultImageUrl} 
-              readmore={element.url} />
+          {
+            this.state.articles.map((element) => {
+
+              let _title = element.title ? element.title.length >= 30 ? element.title.slice(0, 30) + '...' : element.title.slice(0, 30) : 'NA';
+              let _description = element.description ? element.description.length >= 88 ? element.description.slice(0, 88) + '...' : element.description.slice(0, 88) : 'NA';
+              let _imageUrl = element.urlToImage ? element.urlToImage : defaultImageUrl
+
+              return <NewsItem key={element.url} title={_title} description={_description} imageUrl={_imageUrl} readmore={element.url} />
             })
           }
         </div>
