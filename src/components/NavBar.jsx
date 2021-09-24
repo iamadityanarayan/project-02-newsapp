c4930285a321a9d965a54c0d2312c488e62a8faa
Adityanarayan Tiwari <adityant17121997@gmail.com>
2021-09-24 21:02:21 +0530

    function based Spinner updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-const NavBar = () => {
+const NavBar = () => { 
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow fixed-top">
--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,14 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-export default class Spinner extends Component {
-  render() {
-    return (
-      <div className='text-center my-4'>
-        <button className="btn btn-success" type="button" disabled>
-          <span className="spinner-grow spinner-grow-sm me-2" role="status" aria-hidden="true"></span>
-          Loading...
-        </button>
-      </div>
-    )
-  }
+const Spinner = () => {
+  return (
+    <div className='text-center my-4'>
+      <button className="btn btn-success" type="button" disabled>
+        <span className="spinner-grow spinner-grow-sm me-2" role="status" aria-hidden="true"></span>
+        Loading...
+      </button>
+    </div>
+  )
 }
+export default Spinner;
