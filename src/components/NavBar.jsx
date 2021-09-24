8a1af8591c663345264ad8ec0fb53e6e21031af2
Adityanarayan Tiwari <adityant17121997@gmail.com>
2021-09-24 20:57:48 +0530

    function based Navbar updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,31 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
 
-export default class NavBar extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow fixed-top">
-          <div className="container-xxl">
-            <Link className="navbar-brand text-info fw-bold" to="/">NEWS<span className='fst-italic text-warning'>WORLD</span></Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-                <li className="nav-item"><Link className="nav-link text-uppercase" to="/business">business</Link></li>
-                <li className="nav-item"><Link className="nav-link text-uppercase" to="/entertainment">entertainment</Link></li>
-                <li className="nav-item"><Link className="nav-link text-uppercase" to="/general">general</Link></li>
-                <li className="nav-item"><Link className="nav-link text-uppercase" to="/health">health</Link></li>
-                <li className="nav-item"><Link className="nav-link text-uppercase" to="/science">science</Link></li>
-                <li className="nav-item"><Link className="nav-link text-uppercase" to="/sports">sports</Link></li>
-                <li className="nav-item"><Link className="nav-link text-uppercase" to="/technology">technology</Link></li>
-              </ul>
-              <a className="btn btn-sm btn-outline-info" to="#" role="button">Sign Up / Sign In</a>
-            </div>
+const NavBar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow fixed-top">
+        <div className="container-xxl">
+          <Link className="navbar-brand text-info fw-bold" to="/">NEWS<span className='fst-italic text-warning'>WORLD</span></Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+              <li className="nav-item"><Link className="nav-link text-uppercase" to="/business">business</Link></li>
+              <li className="nav-item"><Link className="nav-link text-uppercase" to="/entertainment">entertainment</Link></li>
+              <li className="nav-item"><Link className="nav-link text-uppercase" to="/general">general</Link></li>
+              <li className="nav-item"><Link className="nav-link text-uppercase" to="/health">health</Link></li>
+              <li className="nav-item"><Link className="nav-link text-uppercase" to="/science">science</Link></li>
+              <li className="nav-item"><Link className="nav-link text-uppercase" to="/sports">sports</Link></li>
+              <li className="nav-item"><Link className="nav-link text-uppercase" to="/technology">technology</Link></li>
+            </ul>
+            <a className="btn btn-sm btn-outline-info" to="#" role="button">Sign Up / Sign In</a>
           </div>
-        </nav>
-      </div>
-    )
-  }
+        </div>
+      </nav>
+    </div>
+  )
 }
+
+export default NavBar;
